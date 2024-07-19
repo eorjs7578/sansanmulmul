e6167876257dedb6353c3b7566c9a4d365207f44
곽대건 <eorjs7578@gmail.com>
2024-07-19 16:38:39 +0900

    feat: "회원가입 첫번째 페이지 구현"
    카카오톡 소셜 로그인 창으로 안내하는 회원가입 첫 페이지 구현 완료

--- SANSANMULMUL/app/src/main/java/com/sansantek/sansanmulmul/ui/view/MainActivity.kt
+++ SANSANMULMUL/app/src/main/java/com/sansantek/sansanmulmul/ui/view/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.sansantek.sansanmulmul.R
+import com.sansantek.sansanmulmul.config.BaseActivity
+import com.sansantek.sansanmulmul.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ SANSANMULMUL/app/src/main/java/com/sansantek/sansanmulmul/ui/view/register/RegisterStartFragment.kt
@@ -0,0 +1,27 @@
+package com.sansantek.sansanmulmul.ui.view.register
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.test.databinding.FragmentRegisterStartBinding
+import com.sansantek.sansanmulmul.R
+import com.sansantek.sansanmulmul.databinding.FragmentRegisterStartBinding
+
+class RegisterStartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        val binding = FragmentRegisterStartBinding.inflate(layoutInflater)
+
+        binding.button.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction().replace(R.id.fragment_view, ViewPagerFragment()).addToBackStack(null).commit()
+        }
+
+        return binding.root
+    }
+}
