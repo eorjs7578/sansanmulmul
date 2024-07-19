2819f30751c1d096ac7fc09c1a6d1cb4f2bea417
곽대건 <eorjs7578@gmail.com>
2024-07-19 16:50:15 +0900

    feat: "회원가입 프로필 사진 화면 구현 완료"
    버튼 클릭 로직은 차후 완성 예정

--- SANSANMULMUL/app/src/main/java/com/sansantek/sansanmulmul/ui/view/register/RegisterStartFragment.kt
+++ SANSANMULMUL/app/src/main/java/com/sansantek/sansanmulmul/ui/view/register/RegisterStartFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.test.databinding.FragmentRegisterStartBinding
 import com.sansantek.sansanmulmul.R
 import com.sansantek.sansanmulmul.databinding.FragmentRegisterStartBinding
 
