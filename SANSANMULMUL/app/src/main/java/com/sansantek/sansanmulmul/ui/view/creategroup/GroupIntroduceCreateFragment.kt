b3b720f9404d6842b8809938c9cfeb124e4690bb
곽대건 <eorjs7578@gamil.com>
2024-07-21 23:45:34 +0900

    Update: "화면 상단 색, 그라데이션 적용"

--- SANSANMULMUL/app/src/main/java/com/sansantek/sansanmulmul/ui/view/creategroup/GroupIntroduceCreateFragment.kt
+++ SANSANMULMUL/app/src/main/java/com/sansantek/sansanmulmul/ui/view/creategroup/GroupIntroduceCreateFragment.kt
@@ -5,16 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.sansantek.sansanmulmul.databinding.FragmentGroupCreate1Binding
+import com.sansantek.sansanmulmul.databinding.FragmentGroupIntroduceCreateBinding
 
-class GroupCreateFragment1 : Fragment() {
+class GroupIntroduceCreateFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        val binding = FragmentGroupCreate1Binding.inflate(layoutInflater)
+        val binding = FragmentGroupIntroduceCreateBinding.inflate(layoutInflater)
 
         return binding.root
     }
