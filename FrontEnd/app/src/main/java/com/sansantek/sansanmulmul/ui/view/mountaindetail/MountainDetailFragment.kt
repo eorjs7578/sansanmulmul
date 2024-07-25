392a18e63f9fc82b319921bb1f6cd21794ade70d
minseon <alstjs1516@gmail.com>
2024-07-25 23:04:54 +0900

    S11P12D111-230 | feat: 산 상세 프래그먼트 구현

--- FrontEnd/app/src/main/java/com/sansantek/sansanmulmul/ui/view/mountaindetail/MountainDetailFragment.kt
+++ FrontEnd/app/src/main/java/com/sansantek/sansanmulmul/ui/view/mountaindetail/MountainDetailFragment.kt
@@ -1,5 +1,8 @@
 package com.sansantek.sansanmulmul.ui.view.mountaindetail
 
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.tabs.TabLayout
 import com.sansantek.sansanmulmul.R
 import com.sansantek.sansanmulmul.config.BaseFragment
 import com.sansantek.sansanmulmul.databinding.FragmentMountainDetailBinding
@@ -9,4 +12,23 @@ class MountainDetailFragment : BaseFragment<FragmentMountainDetailBinding>(
   R.layout.fragment_mountain_detail
 ) {
 
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    requireActivity().supportFragmentManager.beginTransaction()
+      .replace(binding.mountainDetailFragmentView.id, MountainDetailTabFirstInfoFragment()).commit()
+
+    binding.layoutTab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+      override fun onTabSelected(tab: TabLayout.Tab?) {
+        requireActivity().supportFragmentManager.beginTransaction()
+          .replace(R.id.mountain_detail_fragment_view, MountainDetailTabFirstInfoFragment())
+          .commit()
+      }
+
+      override fun onTabUnselected(tab: TabLayout.Tab?) {
+      }
+
+      override fun onTabReselected(tab: TabLayout.Tab?) {
+      }
+    })
+  }
 }
---
+++ FrontEnd/app/src/main/java/com/sansantek/sansanmulmul/ui/view/mountaindetail/MountainDetailTabFirstInfoFragment.kt
@@ -0,0 +1,20 @@
+package com.sansantek.sansanmulmul.ui.view.mountaindetail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sansantek.sansanmulmul.R
+import com.sansantek.sansanmulmul.config.BaseFragment
+import com.sansantek.sansanmulmul.databinding.FragmentMountainDetailBinding
+import com.sansantek.sansanmulmul.databinding.FragmentMountainDetailTabFirstInfoBinding
+
+
+class MountainDetailTabFirstInfoFragment : BaseFragment<FragmentMountainDetailTabFirstInfoBinding>(
+  FragmentMountainDetailTabFirstInfoBinding::bind,
+  R.layout.fragment_mountain_detail_tab_first_info
+) {
+
+
+}
