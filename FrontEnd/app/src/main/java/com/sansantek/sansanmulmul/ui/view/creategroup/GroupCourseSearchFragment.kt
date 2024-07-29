a70e1bddc16c71599dfa537af45a32c2eba50846
Hanguelmansae <111sym@naver.com>
2024-07-29 10:24:02 +0900

    fix: "잘못 merge한 것 수정" GroupCourseSearchFragment 부분 잘못 수정한 것 수정

--- FrontEnd/app/src/main/java/com/sansantek/sansanmulmul/ui/view/creategroup/GroupCourseSearchFragment.kt
+++ FrontEnd/app/src/main/java/com/sansantek/sansanmulmul/ui/view/creategroup/GroupCourseSearchFragment.kt
@@ -18,33 +18,21 @@ class GroupCourseSearchFragment : BaseFragment<FragmentGroupCourseSearchBinding>
         super.onCreate(savedInstanceState)
     }
 
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-    init()
-    initSearchView()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initSearchView()
 
-    adapter = GroupCourceSearchListAdapter()
-    binding.rvListSearchMountain.adapter = adapter
-    binding.rvListSearchMountain.layoutManager = LinearLayoutManager(requireContext())
+        adapter = GroupCourceSearchListAdapter { course ->
+            showUpCourseChoiceDialog(course)
+        }
+        binding.rvListSearchMountain.adapter = adapter
+        binding.rvListSearchMountain.layoutManager = LinearLayoutManager(requireContext())
 
-    // 데이터를 어댑터에 전달
-    adapter.submitList(dataList)
+        // 데이터를 어댑터에 전달
+        adapter.submitList(dataList)
 
-  }
+    }
 
-  private fun init() {
-    activity?.let { hideBottomNav(it.findViewById(R.id.main_layout_bottom_navigation), true) }
-  }
-
-  private fun initSearchView() {
-    // SearchView 초기화
-    val searchView = requireView().findViewById<SearchView>(R.id.sv_mountain_searchbar)
-    searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-      override fun onQueryTextSubmit(query: String?): Boolean {
-        // 검색어가 null이 아니고 비어있지 않은 경우
-        if (!query.isNullOrEmpty()) {
-          // 검색어가 데이터 리스트에 존재하는지 확인
-          val isFound = dataList.contains(query)
 
     private fun initSearchView() {
         // SearchView 초기화
@@ -56,22 +44,22 @@ class GroupCourseSearchFragment : BaseFragment<FragmentGroupCourseSearchBinding>
                     // 검색어가 데이터 리스트에 존재하는지 확인
                     val isFound = dataList.contains(query)
 
-          // 검색어가 리스트에 있을 경우
-          if (isFound) {
-            // 검색어에 해당하는 항목만 필터링하여 어댑터에 제출
-            val filteredList = dataList.filter { it == query }
-            adapter.submitList(filteredList) // 검색어와 일치하는 항목만 표시
-            return true
-          } else {
-            // 검색어가 데이터 리스트에 없을 경우, 빈 목록을 어댑터에 제출
-            adapter.submitList(emptyList())
-            return true
-          }
-        }
-        // 검색어가 비어있거나 null일 경우 모든 항목 표시
-        adapter.submitList(dataList)
-        return true
-      }
+                    // 검색어가 리스트에 있을 경우
+                    if (isFound) {
+                        // 검색어에 해당하는 항목만 필터링하여 어댑터에 제출
+                        val filteredList = dataList.filter { it == query }
+                        adapter.submitList(filteredList) // 검색어와 일치하는 항목만 표시
+                        return true
+                    } else {
+                        // 검색어가 데이터 리스트에 없을 경우, 빈 목록을 어댑터에 제출
+                        adapter.submitList(emptyList())
+                        return true
+                    }
+                }
+                // 검색어가 비어있거나 null일 경우 모든 항목 표시
+                adapter.submitList(dataList)
+                return true
+            }
 
             override fun onQueryTextChange(newText: String?): Boolean {
                 // Filter the data based on the search query
