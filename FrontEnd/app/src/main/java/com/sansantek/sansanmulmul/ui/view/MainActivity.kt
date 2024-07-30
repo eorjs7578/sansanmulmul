61c7225b6a88f36450312ccdb0acc0c0ea7c8950
minseon <alstjs1516@gmail.com>
2024-07-31 01:24:38 +0900

    S11P12D111-258 : feat: 비석 사진 찍기 구현

--- FrontEnd/app/src/main/java/com/sansantek/sansanmulmul/ui/view/MainActivity.kt
+++ FrontEnd/app/src/main/java/com/sansantek/sansanmulmul/ui/view/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.sansantek.sansanmulmul.ui.view
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
@@ -28,124 +32,133 @@ private const val TAG = "MainActivity 싸피"
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
-    private lateinit var hikingRecordingTabFragment: HikingRecordingTabFragment
+  private lateinit var hikingRecordingTabFragment: HikingRecordingTabFragment
+  private var photoURI: Uri? = null
+  private lateinit var currentPhotoPath: String
+  private var bitmap: Bitmap? = null
 
-    /** permission check **/
-    private val checker = PermissionChecker(this)
-    private val runtimePermissions =
-        arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+  /** permission check **/
+  private val checker = PermissionChecker(this)
+  private val runtimePermissions =
+    arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
-    fun checkPermission() {
-        if (!checker.checkPermission(this, runtimePermissions)) {
-            checker.setOnGrantedListener { //퍼미션 획득 성공일때
-                openCamera()
-            }
-            checker.requestPermissionLauncher.launch(runtimePermissions) // 권한없으면 창 띄움
-        } else { //이미 전체 권한이 있는 경우
-            openCamera()
+  fun checkPermission() {
+    if (!checker.checkPermission(this, runtimePermissions)) {
+      checker.setOnGrantedListener { //퍼미션 획득 성공일때
+        openCamera()
+      }
+      checker.requestPermissionLauncher.launch(runtimePermissions) // 권한없으면 창 띄움
+    } else { //이미 전체 권한이 있는 경우
+      openCamera()
+    }
+  }
+
+  /** permission check **/
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(binding.root)
+
+    hikingRecordingTabFragment = HikingRecordingTabFragment()
+    initBottomNav()
+    changeFragment(HomeTabFragment())
+
+  }
+
+
+  @SuppressLint("QueryPermissionsNeeded")
+  private fun openCamera() {
+    Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+      takePictureIntent.resolveActivity(packageManager)?.also {
+        val photoFile: File? = try {
+          createImageFile()
+        } catch (ex: IOException) {
+          null
         }
-    }
-
-    /** permission check **/
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-
-        hikingRecordingTabFragment = HikingRecordingTabFragment()
-        initBottomNav()
-        changeFragment(HomeTabFragment())
-
-    }
-
-    private var photoURI: Uri? = null
-
-    private fun openCamera() {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            takePictureIntent.resolveActivity(packageManager)?.also {
-                val photoFile: File? = try {
-                    createImageFile()
-                } catch (ex: IOException) {
-                    null
-                }
-                Log.d(TAG, "openCamera: ph")
-                photoFile?.also {
-                    val photoURI: Uri = FileProvider.getUriForFile(
-                        this,
-                        "com.sansantek.sansanmulmul.fileprovider",
-                        it
-                    )
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-                }
-            }
+        Log.d(TAG, "openCamera: ph")
+        photoFile?.also {
+          photoURI = FileProvider.getUriForFile(
+            this,
+            "com.sansantek.sansanmulmul.fileprovider",
+            it
+          )
+          takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+          startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
         }
+      }
     }
+  }
 
-    lateinit var currentPhotoPath: String
 
-    @Throws(IOException::class)
-    private fun createImageFile(): File {
-        // Create an image file name
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
-        return File.createTempFile(
-            "JPEG_${timeStamp}_", /* prefix */
-            ".jpg", /* suffix */
-            storageDir /* directory */
-        ).apply {
-            // Save a file: path for use with ACTION_VIEW intents
-            currentPhotoPath = absolutePath
+  @Throws(IOException::class)
+  private fun createImageFile(): File {
+    val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+    val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+    return File.createTempFile(
+      "JPEG_${timeStamp}_",
+      ".jpg",
+      storageDir
+    ).apply {
+      currentPhotoPath = absolutePath
+    }
+  }
+
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+    try {
+      if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+        bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+          ImageDecoder.decodeBitmap(ImageDecoder.createSource(contentResolver, photoURI!!))
+        } else {
+          MediaStore.Images.Media.getBitmap(contentResolver, photoURI)
         }
+      }
+    } catch (e: IOException) {
+      val fragment =
+        supportFragmentManager.findFragmentById(R.id.fragment_view) as HikingRecordingTabFragment
+      fragment.setImageBitmap(bitmap)
     }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        Log.d(TAG, "onActivityResult: $data ")
-//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-//            val imageBitmap = data?.extras?.get("data") as Bitmap
-//            hikingRecordingTabFragment.setImageBitmap(imageBitmap)
-//        }
-    }
+  }
 
 
-    private fun initBottomNav() {
-        binding.mainLayoutBottomNavigation.setOnItemSelectedListener {
+  private fun initBottomNav() {
+    binding.mainLayoutBottomNavigation.setOnItemSelectedListener {
 //            Log.d(TAG, "initBottomNav: ${it.itemId}")
-            when (it.itemId) {
-                R.id.home -> {
-                    changeFragment(HomeTabFragment())
-                }
-
-                R.id.map -> {
-                    changeFragment(MapTabFragment())
-                }
-
-                R.id.mountain -> {
-                    changeFragment(HikingRecordingTabFragment())
-                }
-
-                R.id.group -> {
-                    changeFragment(GroupTabFragment())
-                }
-
-                R.id.mypage -> {
-
-                }
-
-            }
-            return@setOnItemSelectedListener true
+      when (it.itemId) {
+        R.id.home -> {
+          changeFragment(HomeTabFragment())
         }
-    }
 
-    fun changeFragment(view: Fragment) {
-        supportFragmentManager.beginTransaction().replace(binding.fragmentView.id, view).commit()
-    }
+        R.id.map -> {
+          changeFragment(MapTabFragment())
+        }
 
-    fun changeAddToBackstackFragment(view: Fragment) {
-        supportFragmentManager.beginTransaction().replace(binding.fragmentView.id, view)
-            .addToBackStack(null).commit()
+        R.id.mountain -> {
+          changeFragment(HikingRecordingTabFragment())
+        }
+
+        R.id.group -> {
+          changeFragment(GroupTabFragment())
+        }
+
+        R.id.mypage -> {
+
+        }
+
+      }
+      return@setOnItemSelectedListener true
     }
+  }
+
+  fun changeFragment(view: Fragment) {
+    supportFragmentManager.beginTransaction().replace(binding.fragmentView.id, view).commit()
+  }
+
+  fun changeAddToBackstackFragment(view: Fragment) {
+    supportFragmentManager.beginTransaction().replace(binding.fragmentView.id, view)
+      .addToBackStack(null).commit()
+  }
 
 
 }
