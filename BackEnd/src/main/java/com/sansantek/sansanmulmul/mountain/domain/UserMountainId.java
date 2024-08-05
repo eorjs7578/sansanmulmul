b6d00bd5f8832af615ed46423babe0c78a3c7724
박태우 <myxodn12@naver.com>
2024-08-05 16:34:13 +0900

    fix: usermountainid 수정

--- BackEnd/src/main/java/com/sansantek/sansanmulmul/mountain/domain/UserMountainId.java
+++ BackEnd/src/main/java/com/sansantek/sansanmulmul/mountain/domain/UserMountainId.java
@@ -4,23 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserMountainId implements Serializable {
     private int user;
-    private Long mountain;
+    private int mountain;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         UserMountainId that = (UserMountainId) o;
-        return user == that.user && mountain.equals(that.mountain);
+        return Objects.equals(user, that.user) && Objects.equals(mountain, that.mountain);
     }
 
     @Override
     public int hashCode() {
-        return 31 * user + mountain.hashCode();
+        return Objects.hash(user, mountain);
     }
 }
