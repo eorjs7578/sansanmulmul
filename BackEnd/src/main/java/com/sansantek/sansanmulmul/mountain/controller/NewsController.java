8c6464861832dba516812ff6784fc1d9cb1c1d28
윤가희 <yunkh2005@naver.com>
2024-08-05 14:02:24 +0900

    feat: 랜덤 뉴스 조회

--- BackEnd/src/main/java/com/sansantek/sansanmulmul/mountain/controller/NewsController.java
+++ BackEnd/src/main/java/com/sansantek/sansanmulmul/mountain/controller/NewsController.java
@@ -9,9 +9,10 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
+import com.sansantek.sansanmulmul.mountain.service.MountainService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,21 +23,23 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/mountain")
+@RequestMapping("/mountain/news")
+@RequiredArgsConstructor
 @CrossOrigin("*")
 @Tag(name = "뉴스 컨트롤러", description = "네이버 산 뉴스를 3개 크롤링")
 @Slf4j
 public class NewsController {
 
+    private final MountainService mountainService;
     @Value("${naver.client-id}")
     private String clientId; // 애플리케이션 클라이언트 아이디
     @Value("${naver.client-secret}")
     private String clientSecret; // 애플리케이션 클라이언트 시크릿
 
 
-    @Operation(summary = "뉴스 정보 요청", description = "네이버 뉴스를 요청")
-    @GetMapping(value = "/news/{keyword}", produces = "application/json; charset=utf-8")
-    public ResponseEntity<String> news(@PathVariable("keyword") String keyword) {
+    @Operation(summary = "뉴스 정보 요청(사용자 기반)", description = "즐겨찾기 산 기반 네이버 뉴스를 요청")
+    @GetMapping(value = "/{keyword}", produces = "application/json; charset=utf-8")
+    public ResponseEntity<String> getUserNews(@PathVariable("keyword") String keyword) {
         String text = null;
         try {
             text = URLEncoder.encode(keyword, "UTF-8");
@@ -54,6 +57,37 @@ public class NewsController {
         System.out.println("뉴스 불러오기 완료");
         System.out.println(responseBody); // 네이버가 주는 결과데이터
         return new ResponseEntity<>(responseBody, HttpStatus.OK);
+    }
+
+    @Operation(summary = "뉴스 정보 요청(랜덤)")
+    @GetMapping(value = "/random", produces = "application/json; charset=utf-8")
+    public ResponseEntity<?> getRandomNews() {
+        // 산 이름들 가져오기
+        List<String> mountainNameList = mountainService.getMountainName();
+
+        // 가져온 산 이름 기반으로 랜덤으로 인덱스 값 5개 뽑기
+        Random random = new Random();
+        Set<String> selectedMountains = new HashSet<>();
+        while (selectedMountains.size() < 5) {
+            int randomIndex = random.nextInt(mountainNameList.size());
+            selectedMountains.add(mountainNameList.get(randomIndex));
+        }
+
+        // 뉴스 불러오기
+        List<String> newsList = new ArrayList<>();
+        for (String mountainName : selectedMountains) {
+            String apiURL = "https://openapi.naver.com/v1/search/news?query=" + mountainName + "&display=1"; // JSON 결과
+
+            Map<String, String> requestHeaders = new HashMap<>();
+            requestHeaders.put("X-Naver-Client-Id", clientId);
+            requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+            String responseBody = get(apiURL, requestHeaders);
+
+            newsList.add(responseBody);
+        }
+
+        System.out.println("뉴스 불러오기 완료");
+        return new ResponseEntity<>(newsList, HttpStatus.OK);
     }
 
     private static String get(String apiUrl, Map<String, String> requestHeaders) {
