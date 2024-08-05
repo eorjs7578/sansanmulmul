ac7e12b0c6213407e15ab1d3bbc59f2ac51a61e6
박태우 <myxodn12@naver.com>
2024-08-05 10:25:49 +0900

    feat :그룹 신청,승인,거절,강퇴기능 구현

---
+++ BackEnd/src/main/java/com/sansantek/sansanmulmul/crew/repository/request/CrewUserRepository.java
@@ -0,0 +1,14 @@
+package com.sansantek.sansanmulmul.crew.repository.request;
+
+import com.sansantek.sansanmulmul.crew.domain.Crew;
+import com.sansantek.sansanmulmul.crew.domain.crewuser.CrewUser;
+import com.sansantek.sansanmulmul.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CrewUserRepository extends JpaRepository<CrewUser, Long> {
+    Optional<CrewUser> findByCrewAndUser(Crew crew, User user);
+}
