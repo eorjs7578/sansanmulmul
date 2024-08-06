2c1019291487f11b7ab791846d51eca846aa29fa
박태우 <myxodn12@naver.com>
2024-08-06 16:09:52 +0900

    fix:BE/crew_request와 BackEnd merge충돌 해결

--- BackEnd/src/main/java/com/sansantek/sansanmulmul/crew/repository/request/CrewUserRepository.java
+++ BackEnd/src/main/java/com/sansantek/sansanmulmul/crew/repository/request/CrewUserRepository.java
@@ -14,15 +14,11 @@ public interface CrewUserRepository extends JpaRepository<CrewUser, Long> {
     Optional<CrewUser> findByCrewAndUser(Crew crew, User user);
 
     List<CrewUser> findByCrew(Crew crew);
-<<<<<<< HEAD
-
     //그룹(crewId)에 참여중인 user 수 가져오기
     int countByCrew_CrewId(int crewId);
 
     //현재 사용자(user)가 그룹(crew)에 참여중인지 여부
     boolean existsByCrewAndUser(Crew crew, User user);
-=======
     List<CrewUser> findByUserAndCrew_CrewIsDone(User user, boolean isDone);
     int countByCrewCrewId(int crewId);
->>>>>>> BE/crew_request
 }
