5bbf17280b9ae974f5e2fdd5e7e06721ee7ce0b8
박태우 <myxodn12@naver.com>
2024-08-10 23:24:17 +0900

    fix:chatcontroller 코드 수정

--- BackEnd/src/main/java/com/sansantek/sansanmulmul/chat/controller/ChatController.java
+++ BackEnd/src/main/java/com/sansantek/sansanmulmul/chat/controller/ChatController.java
@@ -6,23 +6,14 @@ import com.sansantek.sansanmulmul.chat.dto.ChatMessagesResponse;
 import com.sansantek.sansanmulmul.chat.dto.UserMessageResponse;
 import com.sansantek.sansanmulmul.chat.service.ChatService;
 import com.sansantek.sansanmulmul.crew.domain.Crew;
-import com.sansantek.sansanmulmul.crew.service.CrewService;
 import com.sansantek.sansanmulmul.user.domain.User;
 import com.sansantek.sansanmulmul.user.service.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
 
 @Controller
@@ -30,15 +21,18 @@ public class ChatController {
 
     @Autowired
     private ChatService chatService;
+    @Autowired
+    private UserService userService;
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessagesResponse sendMessage(ChatMessageDTO chatMessageDTO) {
+        User user = userService.getUser(chatMessageDTO.getUserId());
         ChatMessage chatMessage = ChatMessage.builder()
                 .messageContent(chatMessageDTO.getMessageContent())
                 .timestamp(LocalDateTime.now())
                 .crew(Crew.builder().crewId(chatMessageDTO.getCrewId()).build())
-                .user(User.builder().userId(chatMessageDTO.getUserId()).build())
+                .user(user)
                 .build();
 
         chatService.saveChatMessage(chatMessage);
