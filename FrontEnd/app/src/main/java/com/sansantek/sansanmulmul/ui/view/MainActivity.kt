de10c440be8df060e34cd9f9ff1f2b3a20127c40
minseon <alstjs1516@gmail.com>
2024-08-12 00:35:34 +0900

    중간커밋3

--- FrontEnd/app/src/main/java/com/sansantek/sansanmulmul/ui/view/MainActivity.kt
+++ FrontEnd/app/src/main/java/com/sansantek/sansanmulmul/ui/view/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.sansantek.sansanmulmul.ui.view
 
 
-import ChatViewModel
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Intent
@@ -18,276 +17,292 @@ import androidx.activity.viewModels
 import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import com.google.gson.GsonBuilder
 import com.sansantek.sansanmulmul.R
 import com.sansantek.sansanmulmul.config.BaseActivity
 import com.sansantek.sansanmulmul.config.Const.Companion.REQUEST_IMAGE_CAPTURE
-import com.sansantek.sansanmulmul.data.model.MessageData
 import com.sansantek.sansanmulmul.databinding.ActivityMainBinding
+import com.sansantek.sansanmulmul.ui.util.PermissionChecker
+import com.sansantek.sansanmulmul.ui.util.RetrofiltUtil.Companion.mountainService
+import com.sansantek.sansanmulmul.ui.util.RetrofiltUtil.Companion.userService
+import com.sansantek.sansanmulmul.ui.util.Util.makeHeaderByAccessToken
 import com.sansantek.sansanmulmul.ui.view.grouptab.GroupTabFragment
 import com.sansantek.sansanmulmul.ui.view.hikingrecordingtab.HikingRecordingTabFragment
 import com.sansantek.sansanmulmul.ui.view.hometab.HomeTabFragment
 import com.sansantek.sansanmulmul.ui.view.maptab.MapTabFragment
 import com.sansantek.sansanmulmul.ui.view.mypagetab.MyPageTabFragment
-import com.sansantek.sansanmulmul.ui.util.PermissionChecker
-import com.sansantek.sansanmulmul.ui.util.RetrofiltUtil.Companion.mountainService
-import com.sansantek.sansanmulmul.ui.util.RetrofiltUtil.Companion.userService
-import com.sansantek.sansanmulmul.ui.util.Util.makeHeaderByAccessToken
 import com.sansantek.sansanmulmul.ui.viewmodel.MainActivityViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
-import java.time.LocalDateTime
 import java.util.Date
+
 
 private const val TAG = "MainActivity 싸피"
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
-    private var photoURI: Uri? = null
-    private lateinit var currentPhotoPath: String
-    private var bitmap: Bitmap? = null
-    private val activityViewModel : MainActivityViewModel by viewModels()
-    private val chatViewModel: ChatViewModel by viewModels()
+  private var photoURI: Uri? = null
+  private lateinit var currentPhotoPath: String
+  private var bitmap: Bitmap? = null
+  private val activityViewModel: MainActivityViewModel by viewModels()
 
-    /** permission check **/
-    private val checker = PermissionChecker(this)
-    private val runtimePermissions =
-        arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+  /** permission check **/
+  private val checker = PermissionChecker(this)
+  private val runtimePermissions =
+    arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
-    fun checkPermission() {
-        if (!checker.checkPermission(this, runtimePermissions)) {
-            checker.setOnGrantedListener { //퍼미션 획득 성공일때
-                openCamera()
-            }
-            checker.requestPermissionLauncher.launch(runtimePermissions) // 권한없으면 창 띄움
-        } else { //이미 전체 권한이 있는 경우
-            openCamera()
-        }
+  fun checkPermission() {
+    if (!checker.checkPermission(this, runtimePermissions)) {
+      checker.setOnGrantedListener { //퍼미션 획득 성공일때
+        openCamera()
+      }
+      checker.requestPermissionLauncher.launch(runtimePermissions) // 권한없으면 창 띄움
+    } else { //이미 전체 권한이 있는 경우
+      openCamera()
+    }
+  }
+
+  /** permission check **/
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(binding.root)
+    lifecycleScope.launch(Dispatchers.IO) {
+      launch(Dispatchers.IO) {
+        loadUserProfile()
+      }
+      launch(Dispatchers.IO) {
+        loadUserHikingStyle()
+      }
+      launch(Dispatchers.IO) {
+        loadFollowInfo()
+      }
+      launch(Dispatchers.IO) {
+        loadLikedMountainList()
+      }
+      launch(Dispatchers.IO) {
+        loadMyPageInfo()
+      }
+      launch(Dispatchers.Main) {
+        initBottomNav()
+      }
+      changeFragment(HomeTabFragment())
     }
 
-    /** permission check **/
+    // QR코드 인식 후
+    val intent = intent
+    if (Intent.ACTION_VIEW == intent.action) {
+      val uri = intent.data
+      if (uri != null) {
+        val fragmentName = uri.getQueryParameter("fragment")
+        if (fragmentName != null) {
+          openFragment(fragmentName)
+        }
+      }
+    }
+  }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+  private fun openFragment(fragmentName: String) {
+    val fragment: Fragment
+    Log.d(TAG, "openFragment: fragmentName = $fragmentName")
 
-//        chatViewModel.runStomp()
-//        val gson = GsonBuilder().create()
-//        val localDate = LocalDateTime.now()
-//        val messageData = MessageData("이번에는 crewId2번으로 보냈어요 진짜에요 -test - 모바일이에요", LocalDateTime.now().toString(), 2, 1)
-//
-//        Log.d(TAG, "onCreate: 보내려는 것 ${gson.toJson(messageData)}")
-//        chatViewModel.stompClient.send("/app/chat.sendMessage",  gson.toJson(messageData).toString()).subscribe()
-        lifecycleScope.launch(Dispatchers.IO) {
-            launch(Dispatchers.IO) {
-                loadUserProfile()
-            }
-            launch(Dispatchers.IO) {
-                loadUserHikingStyle()
-            }
-            launch(Dispatchers.IO) {
-                loadFollowInfo()
-            }
-            launch(Dispatchers.IO) {
-                loadLikedMountainList()
-            }
-            launch(Dispatchers.IO) {
-                loadMyPageInfo()
-            }
+    when (fragmentName) {
+      "HikingRecordingFragment" -> fragment = HikingRecordingTabFragment()
+      else -> fragment = HomeTabFragment()
+    }
+    changeFragment(fragment)
+  }
+
+  private fun loadLikedMountainList() {
+    lifecycleScope.launch(Dispatchers.IO) {
+      activityViewModel.token?.let {
+        launch(Dispatchers.IO) {
+          val likedMountains =
+            mountainService.getLikedMountainList(makeHeaderByAccessToken(it.accessToken))
+          if (likedMountains.code() == 200) {
             launch(Dispatchers.Main) {
-                initBottomNav()
+              likedMountains.body()?.let {
+                activityViewModel.setLikedMountainList(it)
+              }
             }
-            changeFragment(HomeTabFragment())
+          }
         }
+      }
     }
+  }
 
-    private fun loadLikedMountainList(){
-        lifecycleScope.launch(Dispatchers.IO) {
-            activityViewModel.token?.let {
-                launch(Dispatchers.IO) {
-                    val likedMountains = mountainService.getLikedMountainList(makeHeaderByAccessToken(it.accessToken))
-                    if(likedMountains.code() == 200){
-                        launch(Dispatchers.Main){
-                            likedMountains.body()?.let {
-                                activityViewModel.setLikedMountainList(it)
-                            }
-                        }
-                    }
-                }
-            }
+  private fun loadMyPageInfo() {
+    lifecycleScope.launch(Dispatchers.IO) {
+      activityViewModel.token?.let {
+        launch(Dispatchers.IO) {
+          val myPageInfo = userService.getMyPageInfo(makeHeaderByAccessToken(it.accessToken))
+          launch(Dispatchers.Main) {
+            activityViewModel.setMyPageInfo(myPageInfo)
+          }
         }
+      }
     }
+  }
 
-    private fun loadMyPageInfo(){
-        lifecycleScope.launch(Dispatchers.IO) {
-            activityViewModel.token?.let {
-                launch(Dispatchers.IO) {
-                    val myPageInfo = userService.getMyPageInfo(makeHeaderByAccessToken(it.accessToken))
-                    launch (Dispatchers.Main){
-                        activityViewModel.setMyPageInfo(myPageInfo)
-                    }
-                }
-            }
+  private fun loadFollowInfo() {
+    lifecycleScope.launch(Dispatchers.IO) {
+      activityViewModel.token?.let {
+        launch(Dispatchers.IO) {
+          val followerResult = userService.getUserFollower(makeHeaderByAccessToken(it.accessToken))
+          launch(Dispatchers.Main) {
+            activityViewModel.setFollowerList(followerResult)
+          }
         }
-    }
-
-    private fun loadFollowInfo(){
-        lifecycleScope.launch(Dispatchers.IO) {
-            activityViewModel.token?.let {
-                launch(Dispatchers.IO) {
-                    val followerResult = userService.getUserFollower(makeHeaderByAccessToken(it.accessToken))
-                    launch(Dispatchers.Main){
-                        activityViewModel.setFollowerList(followerResult)
-                    }
-                }
-                launch(Dispatchers.IO) {
-                    val followingResult = userService.getUserFollowing(makeHeaderByAccessToken(it.accessToken))
-                    launch(Dispatchers.Main) {
-                        activityViewModel.setFollowingList(followingResult)
-                    }
-                }
-            }
+        launch(Dispatchers.IO) {
+          val followingResult =
+            userService.getUserFollowing(makeHeaderByAccessToken(it.accessToken))
+          launch(Dispatchers.Main) {
+            activityViewModel.setFollowingList(followingResult)
+          }
         }
+      }
     }
+  }
 
-    private fun loadUserProfile(){
-        lifecycleScope.launch(Dispatchers.IO) {
-            activityViewModel.token?.let {
-                val user = userService.loadUserProfile(makeHeaderByAccessToken(it.accessToken))
-                if (user.code() == 200) {
-                    user.body()?.let { usr ->
-                        activityViewModel.setUser(usr)
-                        activityViewModel.setUserProfileImgUrl(usr.userProfileImg)
-                        activityViewModel.setUserTitle(usr.userStaticBadge)
-                    }
-                }
-            }
+  private fun loadUserProfile() {
+    lifecycleScope.launch(Dispatchers.IO) {
+      activityViewModel.token?.let {
+        val user = userService.loadUserProfile(makeHeaderByAccessToken(it.accessToken))
+        if (user.code() == 200) {
+          user.body()?.let { usr ->
+            activityViewModel.setUser(usr)
+            activityViewModel.setUserProfileImgUrl(usr.userProfileImg)
+            activityViewModel.setUserTitle(usr.userStaticBadge)
+          }
         }
+      }
     }
+  }
 
-    private fun loadUserHikingStyle(){
-        activityViewModel.token?.let {
-            lifecycleScope.launch(Dispatchers.Main) {
-                activityViewModel.setHikingStyles(userService.getHikingStyle(makeHeaderByAccessToken(it.accessToken)).sorted())
-            }
+  private fun loadUserHikingStyle() {
+    activityViewModel.token?.let {
+      lifecycleScope.launch(Dispatchers.Main) {
+        activityViewModel.setHikingStyles(
+          userService.getHikingStyle(makeHeaderByAccessToken(it.accessToken)).sorted()
+        )
+      }
+    }
+  }
+
+  @SuppressLint("QueryPermissionsNeeded")
+  private fun openCamera() {
+    Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+      takePictureIntent.resolveActivity(packageManager)?.also {
+        val photoFile: File? = try {
+          createImageFile()
+        } catch (ex: IOException) {
+          null
         }
-    }
-
-    @SuppressLint("QueryPermissionsNeeded")
-    private fun openCamera() {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            takePictureIntent.resolveActivity(packageManager)?.also {
-                val photoFile: File? = try {
-                    createImageFile()
-                } catch (ex: IOException) {
-                    null
-                }
-                Log.d(TAG, "openCamera: ph")
-                photoFile?.also {
-                    photoURI = FileProvider.getUriForFile(
-                        this,
-                        "com.sansantek.sansanmulmul.fileprovider",
-                        it
-                    )
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-                }
-            }
+        Log.d(TAG, "openCamera: ph")
+        photoFile?.also {
+          photoURI = FileProvider.getUriForFile(
+            this,
+            "com.sansantek.sansanmulmul.fileprovider",
+            it
+          )
+          takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+          startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
         }
+      }
     }
+  }
 
 
-    @Throws(IOException::class)
-    private fun createImageFile(): File {
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
-        return File.createTempFile(
-            "JPEG_${timeStamp}_",
-            ".jpg",
-            storageDir
-        ).apply {
-            currentPhotoPath = absolutePath
-        }
+  @Throws(IOException::class)
+  private fun createImageFile(): File {
+    val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+    val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+    return File.createTempFile(
+      "JPEG_${timeStamp}_",
+      ".jpg",
+      storageDir
+    ).apply {
+      currentPhotoPath = absolutePath
     }
+  }
 
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        try {
-            Log.d(TAG, "onActivityResult: 여기를 왜가?")
-            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-                bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                    ImageDecoder.decodeBitmap(
-                        ImageDecoder.createSource(
-                            contentResolver,
-                            photoURI!!
-                        )
-                    )
-                } else {
-                    MediaStore.Images.Media.getBitmap(contentResolver, photoURI)
-                }
-            }
-        } catch (e: IOException) {
-            null
-        }
-        Log.d(TAG, "onActivityResult: $bitmap")
-//        fragment.setImageBitmap(bitmap)
-    }
-
-
-    private fun initBottomNav() {
-
-        binding.mainLayoutBottomNavigation.setOnItemSelectedListener {
-//            Log.d(TAG, "initBottomNav: ${it.itemId}")
-            when (it.itemId) {
-                R.id.home -> {
-                    changeFragment(HomeTabFragment())
-                }
-
-                R.id.map -> {
-                    changeFragment(MapTabFragment())
-                }
-
-                R.id.mountain -> {
-                    changeFragment(HikingRecordingTabFragment())
-                }
-
-                R.id.group -> {
-                    changeFragment(GroupTabFragment())
-                }
-
-                R.id.mypage -> {
-                    changeFragment(MyPageTabFragment())
-                }
-
-            }
-            return@setOnItemSelectedListener true
-        }
-    }
-
-    fun changeFragment(view: Fragment) {
-        for (i in 0 until supportFragmentManager.backStackEntryCount) {
-            supportFragmentManager.popBackStack()
-        }
-        supportFragmentManager.beginTransaction().replace(binding.fragmentView.id, view).commit()
-    }
-
-    fun changeAddToBackstackFragment(view: Fragment) {
-        supportFragmentManager.beginTransaction().replace(binding.fragmentView.id, view)
-            .addToBackStack(null).commit()
-    }
-
-    fun changeBottomNavigationVisibility(visible: Boolean) {
-        if (visible) {
-            binding.mainLayoutBottomNavigation.visibility = View.VISIBLE
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+    try {
+      Log.d(TAG, "onActivityResult: 여기를 왜가?")
+      if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+        bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+          ImageDecoder.decodeBitmap(
+            ImageDecoder.createSource(
+              contentResolver,
+              photoURI!!
+            )
+          )
         } else {
-            binding.mainLayoutBottomNavigation.visibility = View.GONE
+          MediaStore.Images.Media.getBitmap(contentResolver, photoURI)
+        }
+      }
+    } catch (e: IOException) {
+      null
+    }
+    Log.d(TAG, "onActivityResult: $bitmap")
+//        fragment.setImageBitmap(bitmap)
+  }
+
+
+  private fun initBottomNav() {
+
+    binding.mainLayoutBottomNavigation.setOnItemSelectedListener {
+//            Log.d(TAG, "initBottomNav: ${it.itemId}")
+      when (it.itemId) {
+        R.id.home -> {
+          changeFragment(HomeTabFragment())
         }
 
+        R.id.map -> {
+          changeFragment(MapTabFragment())
+        }
+
+        R.id.mountain -> {
+          changeFragment(HikingRecordingTabFragment())
+        }
+
+        R.id.group -> {
+          changeFragment(GroupTabFragment())
+        }
+
+        R.id.mypage -> {
+          changeFragment(MyPageTabFragment())
+        }
+
+      }
+      return@setOnItemSelectedListener true
     }
+  }
+
+  fun changeFragment(view: Fragment) {
+    for (i in 0 until supportFragmentManager.backStackEntryCount) {
+      supportFragmentManager.popBackStack()
+    }
+    supportFragmentManager.beginTransaction().replace(binding.fragmentView.id, view).commit()
+  }
+
+  fun changeAddToBackstackFragment(view: Fragment) {
+    supportFragmentManager.beginTransaction().replace(binding.fragmentView.id, view)
+      .addToBackStack(null).commit()
+  }
+
+  fun changeBottomNavigationVisibility(visible: Boolean) {
+    if (visible) {
+      binding.mainLayoutBottomNavigation.visibility = View.VISIBLE
+    } else {
+      binding.mainLayoutBottomNavigation.visibility = View.GONE
+    }
+
+  }
 
 
 }
