32fa4a11a776b47f7e777d5d4d3e587877a7d9df
Hanguelmansae <111sym@naver.com>
2024-08-13 16:24:21 +0900

    fix: 뷰페이저 dot indicator 클릭 시 바로 페이지 넘어가던 오류 수정
    Former-commit-id: eb3a2fad4132075c8b334bd1cd65f9a5c7353e2d

--- FrontEnd/app/src/main/java/com/sansantek/sansanmulmul/ui/view/creategroup/GroupCreateViewPagerFragment.kt
+++ FrontEnd/app/src/main/java/com/sansantek/sansanmulmul/ui/view/creategroup/GroupCreateViewPagerFragment.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
