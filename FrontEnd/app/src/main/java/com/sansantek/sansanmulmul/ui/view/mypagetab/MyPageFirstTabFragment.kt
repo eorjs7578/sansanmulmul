727a8e96d4eb8fe6967cf31f36ff03d0f844d2c9
minseon <alstjs1516@gmail.com>
2024-08-15 00:13:34 +0900

    중간커밋
    Former-commit-id: 06c798283c9c3aee841ee936a04d10495203920a

--- FrontEnd/app/src/main/java/com/sansantek/sansanmulmul/ui/view/mypagetab/MyPageFirstTabFragment.kt
+++ FrontEnd/app/src/main/java/com/sansantek/sansanmulmul/ui/view/mypagetab/MyPageFirstTabFragment.kt
@@ -1,6 +1,7 @@
 package com.sansantek.sansanmulmul.ui.view.mypagetab
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
@@ -21,79 +22,82 @@ import com.sansantek.sansanmulmul.ui.viewmodel.MainActivityViewModel
 import com.sansantek.sansanmulmul.ui.viewmodel.MountainDetailViewModel
 import kotlinx.coroutines.launch
 
+private const val TAG = "싸피_MyPageFirstTabFragment"
+
 class MyPageFirstTabFragment : BaseFragment<FragmentMyPageFirstTabBinding>(
-    FragmentMyPageFirstTabBinding::bind,
-    R.layout.fragment_my_page_first_tab
+  FragmentMyPageFirstTabBinding::bind,
+  R.layout.fragment_my_page_first_tab
 ) {
-    private val activityViewModel: MainActivityViewModel by activityViewModels()
-    private val mountainDetailViewModel: MountainDetailViewModel by activityViewModels()
-    private var historyMountainList = mutableListOf<MountainHistory>()
+  private val activityViewModel: MainActivityViewModel by activityViewModels()
+  private val mountainDetailViewModel: MountainDetailViewModel by activityViewModels()
+  private var historyMountainList = mutableListOf<MountainHistory>()
 
-    private lateinit var favoriteMountainAdapter: MyPageFirstTabFavoriteMountainListAdapter
-    private lateinit var historyMountainAdapter: MyPageFirstTabHistoryMountainListAdapter
+  private lateinit var favoriteMountainAdapter: MyPageFirstTabFavoriteMountainListAdapter
+  private lateinit var historyMountainAdapter: MyPageFirstTabHistoryMountainListAdapter
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.rvFavoriteMountain.apply {
-            layoutManager =
-                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false).apply {
-                    isMeasurementCacheEnabled = false
-                }
-            favoriteMountainAdapter = MyPageFirstTabFavoriteMountainListAdapter().apply {
-                setItemClickListener(object: MyPageFirstTabFavoriteMountainListAdapter.ItemClickListener{
-                    override fun onClick(mountain: Mountain) {
-                        mountainDetailViewModel.setMountainID(mountain.mountainId)
-                        changeFragmentWithPopUpAnimation(MountainDetailFragment())
-                    }
-                })
-            }
-            loadMyFavoriteMountainList()
-            adapter = favoriteMountainAdapter
-            addItemDecoration(SpaceItemDecoration(30))
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    binding.rvFavoriteMountain.apply {
+      layoutManager =
+        LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false).apply {
+          isMeasurementCacheEnabled = false
         }
-        binding.rvMountainHistory.apply {
-            layoutManager =
-                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false).apply {
-                    isMeasurementCacheEnabled = false
-                }
-            historyMountainAdapter = MyPageFirstTabHistoryMountainListAdapter()
-            historyMountainAdapter.apply {
-                submitList(historyMountainList)
-                setItemClickListener(object :
-                    MyPageFirstTabHistoryMountainListAdapter.ItemClickListener {
-                    override fun onHistoryClick(mountainHistory: MountainHistory) {
-                        ShowMyPageHistoryDialog(mountainHistory).show(childFragmentManager, null)
-                    }
-
-                })
-            }
-            loadMyHikingHistory()
-            adapter = historyMountainAdapter
-            addItemDecoration(SpaceItemDecoration(30))
-        }
+      favoriteMountainAdapter = MyPageFirstTabFavoriteMountainListAdapter().apply {
+        setItemClickListener(object : MyPageFirstTabFavoriteMountainListAdapter.ItemClickListener {
+          override fun onClick(mountain: Mountain) {
+            mountainDetailViewModel.setMountainID(mountain.mountainId)
+            changeFragmentWithPopUpAnimation(MountainDetailFragment())
+          }
+        })
+      }
+      loadMyFavoriteMountainList()
+      adapter = favoriteMountainAdapter
+      addItemDecoration(SpaceItemDecoration(30))
     }
-
-    private fun loadMyFavoriteMountainList() {
-        activityViewModel.token?.let {
-            lifecycleScope.launch {
-                val result =
-                    mountainService.getLikedMountainList(makeHeaderByAccessToken(it.accessToken))
-                if (result.isSuccessful) {
-                    favoriteMountainAdapter.submitList(result.body()!!)
-                }
-            }
+    binding.rvMountainHistory.apply {
+      layoutManager =
+        LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false).apply {
+          isMeasurementCacheEnabled = false
         }
-    }
+      historyMountainAdapter = MyPageFirstTabHistoryMountainListAdapter()
+      historyMountainAdapter.apply {
+        submitList(historyMountainList)
+        setItemClickListener(object :
+          MyPageFirstTabHistoryMountainListAdapter.ItemClickListener {
+          override fun onHistoryClick(mountainHistory: MountainHistory) {
+            ShowMyPageHistoryDialog(mountainHistory).show(childFragmentManager, null)
+          }
 
-    private fun loadMyHikingHistory() {
-        activityViewModel.token?.let {
-            lifecycleScope.launch {
-                val result =
-                    recordService.getUserHikingRecord(activityViewModel.user.userId)
-                if (result.isSuccessful) {
-                    historyMountainAdapter.submitList(result.body()!!)
-                }
-            }
-        }
+        })
+      }
+      loadMyHikingHistory()
+      adapter = historyMountainAdapter
+      addItemDecoration(SpaceItemDecoration(30))
     }
+  }
+
+  private fun loadMyFavoriteMountainList() {
+    activityViewModel.token?.let {
+      lifecycleScope.launch {
+        val result =
+          mountainService.getLikedMountainList(makeHeaderByAccessToken(it.accessToken))
+        if (result.isSuccessful) {
+          favoriteMountainAdapter.submitList(result.body()!!)
+        }
+      }
+    }
+  }
+
+  private fun loadMyHikingHistory() {
+    activityViewModel.token?.let {
+      lifecycleScope.launch {
+        val result =
+          recordService.getUserHikingRecord(activityViewModel.user.userId)
+        if (result.isSuccessful) {
+          Log.d(TAG, "loadMyHikingHistory: 아아아 ${result.body()}")
+          historyMountainAdapter.submitList(result.body()!!)
+        }
+      }
+    }
+  }
 }
